06a7004efd4275e735e0aebf514f44dfa04c9382
onecompileman <stephen.vinuya@gmail.com>
2019-09-10 00:44:30 +0800

    feat: sessions page

---
+++ src/app/main/sessions/sessions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pit-sessions',
+  templateUrl: './sessions.component.html',
+  styleUrls: ['./sessions.component.scss']
+})
+export class SessionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
