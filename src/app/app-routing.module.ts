09b32f59bf2a9d89dd47011c2d42bf000c099688
onecompileman <stephen.vinuya@gmail.com>
2019-09-10 00:40:04 +0800

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: './main/main.module#MainModule'
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
