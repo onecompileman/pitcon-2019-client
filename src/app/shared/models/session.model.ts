219bc72a9065c22b30b935eedcbb51bbc9f52cb0
onecompileman <stephen.vinuya@gmail.com>
2019-10-06 23:52:54 +0800

    refactor: added necessary properties in models

--- src/app/shared/models/session.model.ts
+++ src/app/shared/models/session.model.ts
@@ -1,8 +1,11 @@
+import { Speaker } from './speaker.model';
+
 export class Session {
     time: string;
     title: string;
     description: string;
     speakerId: number;
-    slides: string;
-    video: string;
+    speaker?: Speaker;
+    slides?: string;
+    video?: string;
 }
--- src/app/shared/models/speaker.model.ts
+++ src/app/shared/models/speaker.model.ts
@@ -1,6 +1,7 @@
 export class Speaker {
     id: number;
     name: string;
+    imageUrl: string;
     jobTitle: string;
     about: string;
     aboutJob: string;
