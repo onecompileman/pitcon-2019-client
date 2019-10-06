70b8475fc7abaf64812936a4cc9eea3beabe5274
onecompileman <stephen.vinuya@gmail.com>
2019-10-06 23:52:02 +0800

    feat: added safe-html.pipe for bypassing html string

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpeakerProfileComponent } from './modals/speaker-profile/speaker-profile.component';
 import { ModalModule, AccordionModule } from 'ngx-bootstrap';
+import * as fromPipes from './pipes';
 @NgModule({
   entryComponents: [
     SpeakerProfileComponent
   ],
-  declarations: [SpeakerProfileComponent],
+  declarations: [
+    SpeakerProfileComponent,
+    ...fromPipes.pipes
+  ],
   imports: [
     CommonModule,
     ModalModule.forRoot(),
@@ -14,7 +18,8 @@ import { ModalModule, AccordionModule } from 'ngx-bootstrap';
   ],
   exports: [
     ModalModule,
-    AccordionModule
+    AccordionModule,
+    ...fromPipes.pipes
   ]
 })
 export class SharedModule { }
